2320e378267c8ae7bebb3bfba931c85242f75fce
Annie Fasching <anna.fasching@hotmail.com>
2023-10-30 20:23:16 +0100

    delete unused import statement

--- src/main/java/org/openjfx/dbi/EditCustomerDialogController.java
+++ src/main/java/org/openjfx/dbi/EditCustomerDialogController.java
@@ -9,7 +9,6 @@ import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
